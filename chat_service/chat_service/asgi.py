f31509226690ca3d3d8b61e663d8ed4f50b3e851
Kenneth Luera <kennethpl30.5@gmail.com>
2026-04-09 17:14:42 -0500

    dockerizado

--- chat_service/chat_service/asgi.py
+++ chat_service/chat_service/asgi.py
@@ -1,12 +1,16 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_service.settings')
 
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": URLRouter(
         websocket_urlpatterns
     ),
